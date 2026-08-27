ae7d9aae1ad8c73d6f448eba1f5e5fb8dfbaab75
mbsaharan <45917645+mbsaharan@users.noreply.github.com>
2026-08-27 15:57:44 +0500

    Add build script to execute build process

---
+++ .github/scripts/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "=========================================="
+echo "Executing build script from repo..."
+echo "Current directory: $(pwd)"
+echo "Target environment: $APP_ENV"
+echo "=========================================="
